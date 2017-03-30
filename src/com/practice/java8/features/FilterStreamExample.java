35fad9947a81240fc5bfe80e75cbff77f983cf21
shantanus <shantanu.sahay@gmail.com>
2017-03-30 22:01:31 +0530

    Modified pojo logic

--- src/com/practice/java8/features/FilterStreamExample.java
+++ src/com/practice/java8/features/FilterStreamExample.java
@@ -9,8 +9,8 @@ import java.util.stream.Collectors;
 public class FilterStreamExample {
 
 	public static void main(String[] args) {
-		Employee e1 = new Employee(1001, "Shantanu Sahay", "sahay@gmail.com", 35, LocalDate.now().withYear(1981).withMonth(9).withDayOfMonth(9));
-		Employee e2 = new Employee(1002, "Neha Sahay", "neha@gmail.com", 32, LocalDate.now().withYear(1984).withMonth(12).withDayOfMonth(9));
+		Employee e1 = new Employee(1001, "Sahay", "sahay@gmail.com", 35, LocalDate.now().withYear(1984).withMonth(9).withDayOfMonth(9));
+		Employee e2 = new Employee(1002, "NSahay", "neha@gmail.com", 32, LocalDate.now().withYear(1984).withMonth(12).withDayOfMonth(9));
 		Employee e3 = new Employee(1003, "Aahana Shan", "shan@gmail.com", 6, LocalDate.now().withYear(2011).withMonth(3).withDayOfMonth(27));
 		Employee e4 = new Employee(1004, "Vedandh Shan", "vaashu@gmail.com", 1, LocalDate.now().withYear(2016).withMonth(9).withDayOfMonth(29));
 		
